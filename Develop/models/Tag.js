d28e2362a3bb8f668f1792060627d5801d5ffbf6
Andrey <ashesto121@gmail.com>
2023-05-25 22:04:03 -0700

    imputted code

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,20 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
+const { databaseVersion } = require("../config/connection.js");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+	{
+		// define columns
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		tag_name: {
+			type: Datatypes.STRING,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "tag",
+	}
 );
 
 module.exports = Tag;
